522a0f1f38b57f0458e58ecf84dbaf306f114526
K. Siva Prasad Reddy <skatam@ftdi.com>
2016-07-25 10:24:20 +0530

    Moving springboot archetypes to root folder.

--- springboot-maven-archetypes/quickstart-springboot-angularjs-basic/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-
-
-insert into person(id,email,password,name,dob) values (1,'admin@gmail.com','admin','Administrator','2014-10-12');
-insert into person(id,email,password,name,dob) values (2,'siva@gmail.com','siva','Siva','2014-10-12');
-insert into person(id,email,password,name,dob) values (3,'test@gmail.com','test','TestUser',NULL);
-
-
-
